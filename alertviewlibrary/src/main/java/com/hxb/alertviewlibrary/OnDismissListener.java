731937d642068ea7d227828baa5b3ec3f2dfa503
386985454@qq.com <386985454@qq.com>
2019-04-25 18:59:13 +0800

    更新文件，

---
+++ alertviewlibrary/src/main/java/com/hxb/alertviewlibrary/OnDismissListener.java
@@ -0,0 +1,11 @@
+package com.hxb.alertviewlibrary;
+
+
+/**
+ *  @作者 ：hexingbo
+ *  @时间 ：2019/4/25
+ *  @描述 ：OnDismissListener
+ */
+public interface OnDismissListener {
+    public void onDismiss(Object o);
+}
---
+++ alertviewlibrary/src/main/java/com/hxb/alertviewlibrary/OnItemClickListener.java
@@ -0,0 +1,10 @@
+package com.hxb.alertviewlibrary;
+
+/**
+ *  @作者 ：hexingbo
+ *  @时间 ：2019/4/25
+ *  @描述 ：OnItemClickListener
+ */
+public interface OnItemClickListener {
+    public void onItemClick(Object o, int position);
+}
